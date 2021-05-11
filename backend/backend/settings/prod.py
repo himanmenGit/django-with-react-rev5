47e08c0e9f5c251faaa9991f4e28abf3888b62c4
himanmenGit <study.himanmen@gmail.com>
2021-05-12 01:29:36 +0900

    에듀캐스트 리액트와 함께 장고 시작하기 챕터11) Docker기반으로 AWS/Azure 인프라에 장고 리액트 배포하기

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,1 +1,40 @@
 from .common import *
+
+DEBUG = os.environ.get('DEBUG') in ['1', 't', 'true', 'True', 'T']
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+CORS_ORIGIN_WHITELIST = os.environ.get("CORS_ORIGIN_WHITELIST", "").split(",")
+
+STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
+
+AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+
+# TODO: DataBase
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ["DB_HOST"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'NAME': os.environ["DB_NAME"],
+    }
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "ERROR",
+            "class": "logging.StreamHandler",
+        }
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR"
+        }
+    }
+}
